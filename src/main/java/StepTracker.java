7b968b47a32bc96af9d2586e72664b594df69f6e
Lennier Mulder <lenniermulder@icloud.com>
2025-04-25 20:59:13 -0700

    Attempt 1

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,29 @@
 import java.util.ArrayList;
-public class StepTracker
-{
- /* to be implemented here */
+public class StepTracker {
+    /* to be implemented here */
+    private int min;
+    private ArrayList<Integer> steps = new ArrayList<Integer>();
+
+    public void addDailySteps(int in) {
+        steps.add(in);
+        return;
+    }
+
+    public int activeDays() {
+        int foo = 0;
+        for (int day : steps) {
+            if (day >= min) foo ++;
+        }
+        return foo;
+    }
+
+    public double averageSteps() {
+        double baz = 0;
+        int days = 0;
+        for (int day : steps) {
+            days ++;
+            baz += day;
+        }
+        return (double)(baz) / days;
+    }
 } 
